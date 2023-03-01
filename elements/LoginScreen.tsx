e5a905f85735d391e9a7d642b1e018a03914973c
swojciech35 <104626609+swojciech35@users.noreply.github.com>
2023-03-01 12:17:15 +0100

    add login using google account

--- elements/LoginScreen.tsx
+++ elements/LoginScreen.tsx
@@ -1,8 +1,31 @@
 import {View, Text} from 'react-native';
+import {Button, TextInput} from 'react-native';
+import auth from '@react-native-firebase/auth';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import {GoogleSigninButton} from '@react-native-google-signin/google-signin';
+import React from 'react';
 function LoginScreen(): JSX.Element {
+  const [usr, setUsr] = React.useState<any>(null);
+  async function onGoogleButtonPress() {
+    await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
+    const {idToken} = await GoogleSignin.signIn();
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+    setUsr(auth().currentUser);
+
+    return auth().signInWithCredential(googleCredential);
+  }
+
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text style={{color: 'black'}}>Login screen</Text>
+      <GoogleSigninButton
+        onPress={() =>
+          onGoogleButtonPress().then(() =>
+            console.log('Signed in with Google!'),
+          )
+        }
+      />
+      <Button title="User data" onPress={() => console.log(usr)} />
     </View>
   );
 }
--- index.js
+++ index.js
@@ -5,5 +5,9 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
+GoogleSignin.configure({
+    webClientId: '808189868500-a6ee5k023l90fpc7jdembcu8t4t19fv4.apps.googleusercontent.com',
+  });
 AppRegistry.registerComponent(appName, () => App);
