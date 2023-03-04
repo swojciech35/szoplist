8dcee2824f2a05f72c36ba31fa37c757b63003aa
swojciech35 <104626609+swojciech35@users.noreply.github.com>
2023-03-04 15:48:34 +0100

    refactor store redux app.tsx->index.js

--- App.tsx
+++ App.tsx
@@ -7,27 +7,29 @@ import SelectProductsScreen from './elements/SelectProductsScreen';
 import ShopList from './elements/ShopList';
 import LoginScreen from './elements/LoginScreen';
 import RegisterScreen from './elements/RegisterScreen';
-import store from './redux/store';
-import {Provider} from 'react-redux';
+import {setUser} from './redux/userSlice';
+import {useAppSelector, useAppDispatch} from './hooks';
 const Drawer = createDrawerNavigator();
 
 function App(): JSX.Element {
+  const dispatch = useAppDispatch();
+  React.useEffect(() => {
+    dispatch(setUser({test: 'useeffect'}));
+  }, []);
   return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <Drawer.Navigator initialRouteName="Home">
-          <Drawer.Screen name="Home" component={HomeScreen} />
-          <Drawer.Screen name="Create New List" component={CreateShopList} />
-          <Drawer.Screen
-            name="Select products screen"
-            component={SelectProductsScreen}
-          />
-          <Drawer.Screen name="Show List" component={ShopList} />
-          <Drawer.Screen name="Login" component={LoginScreen} />
-          <Drawer.Screen name="Register" component={RegisterScreen} />
-        </Drawer.Navigator>
-      </NavigationContainer>
-    </Provider>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Create New List" component={CreateShopList} />
+        <Drawer.Screen
+          name="Select products screen"
+          component={SelectProductsScreen}
+        />
+        <Drawer.Screen name="Show List" component={ShopList} />
+        <Drawer.Screen name="Login" component={LoginScreen} />
+        <Drawer.Screen name="Register" component={RegisterScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
--- index.js
+++ index.js
@@ -5,9 +5,17 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
-
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import store from './redux/store';
+import {Provider} from 'react-redux';
 GoogleSignin.configure({
-    webClientId: '808189868500-a6ee5k023l90fpc7jdembcu8t4t19fv4.apps.googleusercontent.com',
-  });
-AppRegistry.registerComponent(appName, () => App);
+  webClientId:
+    '808189868500-a6ee5k023l90fpc7jdembcu8t4t19fv4.apps.googleusercontent.com',
+});
+
+const RNRedux = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+AppRegistry.registerComponent(appName, () => RNRedux);
