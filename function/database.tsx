5ad1917676725903d5b7be59c679a12f62a6cecd
swojciech35 <104626609+swojciech35@users.noreply.github.com>
2023-03-07 20:19:42 +0100

    get Users from database

--- function/database.tsx
+++ function/database.tsx
@@ -1,15 +1,22 @@
 import {firebase} from '@react-native-firebase/database';
 
+const databaseConnect = firebase
+  .app()
+  .database('https://szoplist-e9866-default-rtdb.firebaseio.com/');
 export const addNewUserToDatabase = (
   id: string | undefined,
   email: string | undefined | null,
 ) => {
-  firebase
-    .app()
-    .database('https://szoplist-e9866-default-rtdb.firebaseio.com/')
-    .ref(`/user/${id}`)
-    .set({
-      id: id,
-      email: email,
-    });
+  databaseConnect.ref(`/user/${id}`).set({
+    id: id,
+    email: email,
+  });
 };
+
+export const getUsers = () =>
+  databaseConnect
+    .ref('/user')
+    .once('value')
+    .then(snapshot => {
+      return snapshot.val();
+    });
