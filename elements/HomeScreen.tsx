0495c8ec3bfa3cf1ea0d76895357fe5d2109a8c0
xkasiaaa3 <104648382+xkasiaaa3@users.noreply.github.com>
2023-03-17 19:57:32 +0100

    create shoplist screen look

--- elements/HomeScreen.tsx
+++ elements/HomeScreen.tsx
@@ -5,15 +5,21 @@ import DrawerShowButton from './element/DrawerShowButton';
 
 function HomeScreen({navigation}: any): JSX.Element {
   return (
-    <><View><DrawerShowButton navigation={navigation} /></View><View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={{ color: 'black' }}>Home Screen</Text>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate('Create New List');
-        } }>
-        <Text>Utwórz nową listę</Text>
-      </TouchableOpacity>
-    </View></>
+    <>
+      <View>
+        <DrawerShowButton navigation={navigation} />
+      </View>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text style={{color: 'black'}}>Home Screen</Text>
+        <TouchableOpacity
+          onPress={() => {
+            // navigation.navigate('Create New List');
+            navigation.navigate('Show List');
+          }}>
+          <Text>Utwórz nową listę</Text>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 export default HomeScreen;
