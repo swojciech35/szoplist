0033c4d0bbb636c7ea1e4c78ed17e0921a7bc80e
swojciech35 <104626609+swojciech35@users.noreply.github.com>
2023-03-18 14:42:55 +0100

    add sharedListId to redux

--- redux/listSlice.tsx
+++ redux/listSlice.tsx
@@ -3,13 +3,17 @@ export const listSlice = createSlice({
   name: 'list',
   initialState: {
     listId: null,
+    sharedListId: null,
   },
   reducers: {
     setListId: (state, action) => {
       state.listId = action.payload;
     },
+    setSharedListId: (state, action) => {
+      state.sharedListId = action.payload;
+    },
   },
 });
 
-export const {setListId} = listSlice.actions;
+export const {setListId, setSharedListId} = listSlice.actions;
 export default listSlice.reducer;
